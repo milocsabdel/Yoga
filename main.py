f5233499c82006ac9f4f140cda5308592937b12c
abdel <104756816+milocsabdel@users.noreply.github.com>
2022-05-10 10:04:57 +0000

    post_new

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 from replit import db
 
 app = Flask('app')
+db["eleves"] = [{
+  "prenom": "fadoua",
+  "animal préféré": "chat"
+}, {
+  "prenom": "kawtar",
+  "aniùmal prréfére": "renard"
+}, {
+  "prenom": "hanna",
+  "animal préfére": "lion"        
+}]
 
 db["posts"] = [{
   "title": "la recette de spaghettis",
@@ -23,6 +33,10 @@ def index():
 def contact():
   return render_template("contact.html")
 
+@app.route('/post/new')
+def post_new():
+  return render_template("post_new.html")
+
 @app.route('/team')
 def team():
   return render_template("team.html")
@@ -34,6 +48,15 @@ def posts():
 @app.route('/posts/<id>')
 def show(id):
   return render_template("show.html", post=db["posts"][int(id)])
-    
+
+@app.route('/eleve')
+def eleve():
+  return render_template("eleves.html", posts=db["eleve"])
+               
+@app.route('/eleve/<id>')
+def showeleves(id):
+  return render_template("showeleves.html", eleve=db["eleves"][int(id)])
+
+
   
 app.run(host='0.0.0.0', port=8080)
