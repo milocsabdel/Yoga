5c743bf76867094ab2f5d77169f54356ec1297ad
abdel <104756816+milocsabdel@users.noreply.github.com>
2022-06-27 12:22:39 +0000

    completely new and different

--- main.py
+++ main.py
@@ -1,54 +1,28 @@
-from flask import Flask, render_template, request
-from replit import db
+from flask import Flask, render_template
 
-app = Flask('app')
+app = Flask(__name__)
 
-  
-              
 @app.route('/')
-def index():
-  return render_template("index.html")
+def home():
+  return render_template("home.html")
 
-  
 @app.route('/contact')
 def contact():
   return render_template("contact.html")
-
-
-@app.route('/team')
-def team():
-  return render_template("team.html")
   
-@app.route('/posts')
-def posts():
-  return render_template("posts.html", posts=db["posts"])
+@app.route('/nos-prix')
+def nos_prix():
+  return render_template("nos-prix.html")
+
+@app.route('/notre-equipe')
+def notre_equipe():
+  return render_template("notre-equipe.html")
+
+@app.route('/la-section-des-enfants')
+def la_section_des_enfants():
+  return render_template("la-section-des-enfants.html")
 
 
-@app.route('/post/new')
-def post_new():
-  return render_template("post_new.html")
+app.run(host='0.0.0.0', port=81)
 
 
-@app.route('/post/create', methods=['POST'])
-def create():
-  db['posts'].append({
-   'titre': request.form["title"],
-   'content':request.form["content"]
- })
-  return render_template("index.html")
-               
-@app.route('/posts/<id>')
-def show(id):
-  return render_template("show.html", post=db["posts"][int(id)])
-
-@app.route('/eleve')
-def eleve():
-  return render_template("eleves.html", posts=db["eleve"])
-               
-@app.route('/eleve/<id>')
-def showeleves(id):
-  return render_template("showeleves.html", eleve=db["eleves"][int(id)])
-
-
-  
-app.run(host='0.0.0.0', port=8080)
